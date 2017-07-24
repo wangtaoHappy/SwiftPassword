7ebdf0946ba9e4f0dcdb1abb688ab8d09698f0c5
王涛 <1315455680@qq.com>
2017-07-24 11:22:20 +0800

    Initial Commit

---
+++ SwiftPassword/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftPassword
+//
+//  Created by 王涛 on 2017/7/24.
+//  Copyright © 2017年 王涛. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
